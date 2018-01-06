a60fec94c7eb149a17d95b8eccb20d4ba8dc6545
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-01-06 14:32:38 +0100

    Add Artist, index, show, destroy

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,8 @@
 class Artist < ApplicationRecord
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :image_url, length: { maximum: 255 }
+
   def self.order_by_name
     order(:name)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "artists#index"
 
-  get "artists" => "artists#index"
+  resources :artists, only: [:index, :show, :destroy]
+  # get "artists" => "artists#index"
+  # get "artists/:id" => "artists#show", as: :artist
+  # delete "artists/:id" => "artists#destroy"
 
 end
